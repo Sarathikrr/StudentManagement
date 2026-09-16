06a98daa1983fb00907b919a0f503065c43ba4bd
Sarathi <sarathi@gmail.com>
2026-09-16 23:41:09 +0530

    Connect frontend to live backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api/students/";
+const API_URL = "https://studentmanagementfc.onrender.com/api/students/";
 
 const form = document.getElementById("studentForm");
 const table = document.getElementById("studentTable");
